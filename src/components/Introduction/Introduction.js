725e806ccfabe8a1d57ef8286534bb000cc192df
Victor Santos <victorpss@dcc.ufrj.br>
2024-03-04 23:28:28 -0300

    added home background

---
+++ src/components/Introduction/Introduction.js
@@ -0,0 +1,15 @@
+// Create a new component, let's call it Introduction.js
+
+// Introduction.js
+import React from 'react';
+import './Introduction.css'; // Create this CSS file for styling
+
+const Introduction = () => {
+  return (
+    <div className="gradient-container">
+      {/* Content within the gradient background area goes here */}
+    </div>
+  );
+};
+
+export default Introduction;
