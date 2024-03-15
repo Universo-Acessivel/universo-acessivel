bd9bb18183fce11b50d9c6c214ef7a2bafc30183
Victor Santos <victorpss@dcc.ufrj.br>
2024-03-15 02:07:41 -0300

    added description and files section

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -3,12 +3,16 @@ import logo from '../.././logo.svg';
 import '../.././styles/fonts.css';
 import Header from '.././Header/Header.js';
 import Introduction from '.././Introduction/Introduction.js';
+import Description from '.././Description/Description.js';
+import Files from '.././Files/Files.js';
 
 function Home() {
   return (
     <div>
       <Header />
       <Introduction />
+      <Description />
+      <Files />
       <div style={{ height: "1000px" }}> </div>
     </div>
   );
