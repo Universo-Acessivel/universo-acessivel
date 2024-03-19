8cb1b40a55d568ce3e9066e7c025f4d8f3672c0e
Victor Santos <victorpss@dcc.ufrj.br>
2024-03-18 23:37:40 -0300

    feat: footer

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Footer.css';
+
+function Footer() {
+    return (
+      <div className='footer-container'> 
+        <div className='footer-text'>
+            Copyright © 2024 Universo Acessível
+        </div>
+      </div>
+    );
+  }
+  
+  export default Footer;
