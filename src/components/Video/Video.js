c0a6147aa8ea368b4a6d86966e8298292af7e943
victorpss <victorpss@dcc.ufrj.br>
2024-04-04 21:18:25 -0300

    feat: check out our videos

---
+++ src/components/Video/Video.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './Video.css';
+
+function Video() {
+    return (
+        <React.Fragment>
+            <div className='video-container'>
+                <div className='video-text'>
+                    <div className='video-title'>Confira nossos vídeos no YouTube</div>
+                    <div className='video-description'>Alguns dos nossos conteúdos estão disponíveis no YouTube. Inscreva-se e explore nosso acervo de vídeos!</div>
+                </div>
+                <iframe id='YT-video' src="https://www.youtube.com/embed/drZg5yoYHqk?si=qnypPDpnv-rOL6Xw" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+            </div>
+        </React.Fragment>
+    );
+  }
+  
+  export default Video;
