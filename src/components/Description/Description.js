61c4388afa6d431c944b33b92fb5c1e317b67d5c
victorpss <victorpss@dcc.ufrj.br>
2024-04-08 19:55:01 -0300

    feat: real project description

--- src/components/Description/Description.js
+++ src/components/Description/Description.js
@@ -5,19 +5,15 @@ function Description() {
   return (
     <div className='description-container'>
         <div className='project-description'>
-            “Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
-            sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
-            Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris 
-            nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in 
-            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla 
-            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa 
-            qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit 
-            amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut 
-            labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud 
-            exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
-            Duis aute irure dolor in reprehenderit in voluptate velit esse cillum 
-            dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-            proident, sunt in culpa qui officia deserunt mollit anim id est laborum.”
+            “O Universo Acessível tem enfoque na produção de recursos didáticos adaptados em diferentes 
+            formatos servindo de apoio para alunos do Ensino Fundamental com deficiência visual, buscando 
+            estimular o conhecimento nessa área. Desenvolvemos objetos 3D, feitos com material de baixo 
+            custo que podem ser replicados mediante instruções disponibilizadas pelo projeto, cadernos táteis, 
+            jogos e livros falados. O público-alvo de nossa ação são pessoas cegas e com baixa visão, em especial 
+            os alunos do <a id="link-IBC" href="https://www.gov.br/ibc/pt-br" target='blank' referrerPolicy='no-referrer'>Instituto Benjamin Constant (IBC)</a>. No entanto, atingimos estudantes de todo o Brasil, 
+            uma vez que o IBC distribui o material criado pelo nosso grupo. Testes iniciais, realizados em sala 
+            de aula mostram a eficiência na utilização desse material como apoio para o ensino de astronomia e 
+            motivador para estudantes seguirem a carreira em ciências.”
         </div>
         <div className='teacher-info'>
             Silvia Lorenz-Martins
