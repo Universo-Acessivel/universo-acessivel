9d14a539bf4c17916957a60a395126ec54a9c26d
Victor Santos <104112503+victorpss@users.noreply.github.com>
2024-07-23 13:12:47 -0300

    Update Files.js

--- src/components/Files/Files.js
+++ src/components/Files/Files.js
@@ -10,8 +10,8 @@ import Article from '../.././assets/Article-Icon.svg'
 function Files(){
   return (
     /*<div className='files-wrapper'>
-      <div className='files-title'>Materiais</div>*/
-    <React.Fragment>/* actual - esse e a linha de baixo */
+      <div className='files-title'>Materiais</div> actual: as 2 linhas de baixo*/
+    <React.Fragment>
       <div className='files-title section-title'>Materiais</div> 
         <div className='files-container'>
           <Material 
