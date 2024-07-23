e64c867703eaa03007ee42d4ada967370667ab9a
victorpss <victorpss@dcc.ufrj.br>
2024-07-23 15:49:43 -0300

    add id to section & title to link

--- src/components/Description/Description.js
+++ src/components/Description/Description.js
@@ -3,14 +3,14 @@ import './Description.css';
 
 function Description() {
   return (
-    <div className='description-container'>
+    <div id="sobreNos" className='description-container'>
         <div className='project-description'>
             “O Universo Acessível tem enfoque na produção de recursos didáticos adaptados em diferentes 
             formatos servindo de apoio para alunos do Ensino Fundamental com deficiência visual, buscando 
             estimular o conhecimento nessa área. Desenvolvemos objetos 3D, feitos com material de baixo 
             custo que podem ser replicados mediante instruções disponibilizadas pelo projeto, cadernos táteis, 
             jogos e livros falados. O público-alvo de nossa ação são pessoas cegas e com baixa visão, em especial 
-            os alunos do <a className="link-IBC" href="https://www.gov.br/ibc/pt-br" target='blank' referrerPolicy='noopener noreferrer'>Instituto Benjamin Constant (IBC)</a>. No entanto, atingimos estudantes de todo o Brasil, 
+            os alunos do <a className="link-IBC" href="https://www.gov.br/ibc/pt-br" target='blank' referrerPolicy='noopener noreferrer' title='Acessar Instituto Benjamin Constant'>Instituto Benjamin Constant (IBC)</a>. No entanto, atingimos estudantes de todo o Brasil, 
             uma vez que o IBC distribui o material criado pelo nosso grupo. Testes iniciais, realizados em sala 
             de aula mostram a eficiência na utilização desse material como apoio para o ensino de astronomia e 
             motivador para estudantes seguirem a carreira em ciências.”
