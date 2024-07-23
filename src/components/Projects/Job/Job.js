b0afbb9a9d759671e37bf2ec37ea088f3168a727
victorpss <victorpss@dcc.ufrj.br>
2024-07-23 15:51:37 -0300

    feat: link to projects

--- src/components/Projects/Job/Job.js
+++ src/components/Projects/Job/Job.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import './Job.css';
 
-function Job({ imgSrc, alt, title, description }) {
+function Job({ imgSrc, alt, title, description, link }) {
     return (
       <div className='job-container'> 
             <img id="job-image" src={imgSrc} alt={alt} />
             <div className='job-title subsection-title'>
-                {title}
+                {link ? (
+                    <a href={link} target='_blank' rel='noreferrer noopener' title={`Ver ${title}`}>{title}</a>
+                ) : (
+                    <span>{title}</span>
+                )}
             </div>
             <div className='job-description section-description'>
                 {description}
