f3006d7a245cc9ecec2e7c1bf4fce0012805d4c2
0xGUZ <gustavovm@dcc.ufrj.br>
2024-12-02 18:13:25 -0300

    feat: add videos

--- src/components/Video/Video.js
+++ src/components/Video/Video.js
@@ -32,6 +32,13 @@ function Video() {
                     >
                         Alguns dos nossos conteúdos estão disponíveis no YouTube. Inscreva-se e explore nosso acervo de vídeos!
                     </div>
+                    <div 
+                        className='video-description section-description'
+                        onClick={() => handleTextRead('Você pode encontrar todos os nossos conteúdos aqui!')}
+                        onMouseEnter={() => handleTextRead('Você pode encontrar todos os nossos conteúdos aqui!')}
+                    >
+                        Você pode encontrar todos os nossos conteúdos <a href="https://drive.google.com/drive/folders/1DB2tin4sbTMt5KBpCSzZEDf0YkzyhAZo?usp=sharing">aqui!</a>
+                    </div>
                 </div>
                 <iframe id='YT-video' src="https://www.youtube.com/embed/drZg5yoYHqk?si=qnypPDpnv-rOL6Xw" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
             </div>
