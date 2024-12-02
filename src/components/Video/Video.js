f596c4d1f671eae626e112fb606c00852e4c2bac
0xGUZ <gustavovm@dcc.ufrj.br>
2024-12-02 18:26:57 -0300

    feat: video description

--- src/components/Video/Video.js
+++ src/components/Video/Video.js
@@ -34,10 +34,10 @@ function Video() {
                     </div>
                     <div 
                         className='video-description section-description'
-                        onClick={() => handleTextRead('Você pode encontrar todos os nossos conteúdos aqui!')}
-                        onMouseEnter={() => handleTextRead('Você pode encontrar todos os nossos conteúdos aqui!')}
+                        onClick={() => handleTextRead('Você pode encontrar e baixar todos os nossos conteúdos aqui!')}
+                        onMouseEnter={() => handleTextRead('Você pode encontrar e baixar todos os nossos conteúdos aqui!')}
                     >
-                        Você pode encontrar todos os nossos conteúdos <a href="https://drive.google.com/drive/folders/1DB2tin4sbTMt5KBpCSzZEDf0YkzyhAZo?usp=sharing">aqui!</a>
+                        Você pode encontrar e baixar todos os nossos conteúdos <a href="https://drive.google.com/drive/folders/1DB2tin4sbTMt5KBpCSzZEDf0YkzyhAZo?usp=sharing">aqui!</a>
                     </div>
                 </div>
                 <iframe id='YT-video' src="https://www.youtube.com/embed/drZg5yoYHqk?si=qnypPDpnv-rOL6Xw" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
