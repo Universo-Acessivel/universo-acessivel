6b7b94e803818ebcad4f87c1bedbe6c7e41c70f2
MDuqi <matheusccbr@gmail.com>
2025-09-23 15:15:32 -0300

    feat: update youtube link in Video.js

--- src/components/Video/Video.js
+++ src/components/Video/Video.js
@@ -37,7 +37,7 @@ function Video() {
                         onClick={(e) => handleTextRead(e.currentTarget.textContent)}
                         onMouseEnter={(e) => handleTextRead(e.currentTarget.textContent)}
                     >
-                        Você pode encontrar e baixar todos os nossos conteúdos <a href="https://drive.google.com/drive/folders/1DB2tin4sbTMt5KBpCSzZEDf0YkzyhAZo?usp=sharing">aqui!</a>
+                        Você pode encontrar todos os nossos conteúdos <a href="https://www.youtube.com/channel/UCc-2Cv4UehDOxb4j1MI4R-w">aqui!</a>
                     </div>
                 </div>
                 <iframe id='YT-video' src="https://www.youtube.com/embed/drZg5yoYHqk?si=qnypPDpnv-rOL6Xw" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
